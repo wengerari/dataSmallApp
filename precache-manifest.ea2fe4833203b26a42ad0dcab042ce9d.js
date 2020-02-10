169a736232f76e4a5f0e068b5806a4ae19c4a17b
wengerari <wengerari@gmail.com>
2020-02-10 12:31:15 +0300

    Updates

---
+++ precache-manifest.ea2fe4833203b26a42ad0dcab042ce9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26b1cc1178e39f0ee5fd53f784b95149",
+    "url": "/dataSmallApp/index.html"
+  },
+  {
+    "revision": "47ee1c57b8f00b353306",
+    "url": "/dataSmallApp/static/css/main.69e6c3a0.chunk.css"
+  },
+  {
+    "revision": "75f9650ee3bede356ccc",
+    "url": "/dataSmallApp/static/js/2.90b521fd.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dataSmallApp/static/js/2.90b521fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47ee1c57b8f00b353306",
+    "url": "/dataSmallApp/static/js/main.4b00dea2.chunk.js"
+  },
+  {
+    "revision": "c4074233c5ee105428cd",
+    "url": "/dataSmallApp/static/js/runtime-main.ce2e010d.js"
+  }
+]);
